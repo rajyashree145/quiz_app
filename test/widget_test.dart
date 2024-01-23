9d0739267722440dec12b86665d0794dbd72fb69
rajyashree145 <rajyashree145@gmail.com>
2024-01-23 16:54:04 +0530

    commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:quiz_app/quiz.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(Quiz());
+    await tester.pumpWidget(const Quiz());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
@@ -26,4 +26,5 @@ void main() {
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
+
 }
